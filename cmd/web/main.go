a5581fb7aab395862045010247cf889a047766b1
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-09-02 01:44:46 +0600

    application ready

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 )
 
 type application struct {
@@ -20,5 +21,23 @@ type server struct {
 }
 
 func main() {
+	server := server{
+		host: "localhost",
+		port: "8090",
+		url:  "http://localhost:8090",
+	}
+
+	app := application{
+		server:  server,
+		appName: "Golang News Portal",
+		debug:   true,
+		infoLog: log.New(os.Stdout, "INFO\t", log.Ltime|log.Ldate|log.Lshortfile),
+		errLog:  log.New(os.Stdout, "ERROR\t", log.Ltime|log.Ldate|log.Llongfile),
+	}
+
+	if err := app.listenAndServer(); err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Hello world!")
 }
