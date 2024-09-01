6f979f45d2715d310c4e6cc9dcc2f506f85321ae
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-09-02 02:04:24 +0600

    jet template engine added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/CloudyKit/jet/v6"
 	"log"
 	"os"
 )
@@ -12,6 +13,7 @@ type application struct {
 	debug   bool
 	errLog  *log.Logger
 	infoLog *log.Logger
+	view    *jet.Set
 }
 
 type server struct {
@@ -33,6 +35,12 @@ func main() {
 		debug:   true,
 		infoLog: log.New(os.Stdout, "INFO\t", log.Ltime|log.Ldate|log.Lshortfile),
 		errLog:  log.New(os.Stdout, "ERROR\t", log.Ltime|log.Ldate|log.Llongfile),
+	}
+
+	if app.debug {
+		app.view = jet.NewSet(jet.NewOSFileSystemLoader("./views"), jet.InDevelopmentMode())
+	} else {
+		app.view = jet.NewSet(jet.NewOSFileSystemLoader("./views"))
 	}
 
 	if err := app.listenAndServer(); err != nil {
