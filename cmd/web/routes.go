741aeda31f957a3dde5d0efb6f92fbd9190029be
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-09-02 01:49:58 +0600

    middleware added in routes file

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func (a *application) routes() http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.Recoverer)
+
+	if a.debug {
+		mux.Use(middleware.Logger)
+	}
+
 	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(a.appName))
+	})
+	mux.Get("/comments", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("comments"))
 	})
 
 	return mux
