cdb04434aab4f360f3b4a102247b8afc84097474
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-09-11 01:54:28 +0600

    homeHandler added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/CloudyKit/jet/v6"
 	"github.com/RakibSiddiquee/golang-news-portal/models"
 	"log"
 	"net/http"
@@ -9,13 +11,36 @@ import (
 func (a *application) homeHandler(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
-		log.Fatal(err)
+		a.serverError(w, err)
+		return
 	}
 
 	filter := models.Filter{
-		Query:    "",
-		Page:     1,
-		PageSize: 1,
-		OrderBy:  "",
+		Query:    r.URL.Query().Get("q"),
+		Page:     a.readIntDefault(r, "page", 1),
+		PageSize: a.readIntDefault(r, "page_size", 5),
+		OrderBy:  r.URL.Query().Get("order_by"),
+	}
+
+	posts, meta, err := a.Models.Posts.GetAll(filter)
+	if err != nil {
+		a.serverError(w, err)
+		return
+	}
+
+	queryUrl := fmt.Sprintf("page_size=%d&order_by=%s&q=%s", meta.PageSize, filter.OrderBy, filter.Query)
+	nextUrl := fmt.Sprintf("%s&page=%d", queryUrl, meta.NextPage)
+	prevUrl := fmt.Sprintf("%s&page=%d", queryUrl, meta.PrevPage)
+
+	vars := make(jet.VarMap)
+	vars.Set("posts", posts)
+	vars.Set("meta", meta)
+	vars.Set("nextUrl", nextUrl)
+	vars.Set("prevUrl", prevUrl)
+
+	err = a.render(w, r, "index", vars)
+
+	if err != nil {
+		log.Fatal(err)
 	}
 }
